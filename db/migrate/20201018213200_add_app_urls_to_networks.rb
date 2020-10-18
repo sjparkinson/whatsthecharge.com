ae145f9cc0384c5d9c59d0374ab05a6686796288
Samuel Parkinson <samuel.parkinson@ft.com>
2020-10-18 22:58:01 +0000

    Rename app IDs to URLs

--- db/migrate/20201018213200_add_app_urls_to_networks.rb
+++ db/migrate/20201018213200_add_app_urls_to_networks.rb
@@ -1,9 +1,6 @@
 class AddAppUrlsToNetworks < ActiveRecord::Migration[6.0]
   def change
-    add_column :networks, :android_app_id, :string
-    add_column :networks, :ios_app_id, :string
-
-    add_index :networks, :android_app_id, unique: true
-    add_index :networks, :ios_app_id, unique: true
+    add_column :networks, :android_app_url, :string
+    add_column :networks, :ios_app_url, :string
   end
 end
