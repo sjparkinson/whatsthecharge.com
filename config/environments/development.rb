00a3f066a850b5c98edfa7e4b5cfe12d98912f49
Samuel Parkinson <samuel.parkinson@ft.com>
2020-10-21 16:36:58 +0000

    Simplify cookie_store configuration

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,9 +51,4 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-
-  # Clear hosts to work with GitHub Codespaces urls in development
-  config.hosts.clear
-
-  config.action_controller.default_protect_from_forgery = false
 end
