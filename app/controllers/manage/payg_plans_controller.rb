04b721626ef5851cfa22879f236396ef3fe27a4d
Samuel Parkinson <samuel.parkinson@ft.com>
2020-11-06 19:35:37 +0000

    Order things and add a count

--- app/controllers/manage/payg_plans_controller.rb
+++ app/controllers/manage/payg_plans_controller.rb
@@ -2,7 +2,7 @@ class Manage::PaygPlansController < Manage::ManageController
   before_action :set_payg_plan, only: %i[edit update destroy]
 
   def index
-    @payg_plans = PaygPlan.all
+    @payg_plans = PaygPlan.all.order(network_id: :desc)
   end
 
   def new
