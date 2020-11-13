41c4e62c4989d809cbe702f482da6f4007144926
Samuel Parkinson <samuel.parkinson@ft.com>
2020-11-13 16:36:58 +0000

    Order the networks list by name

--- app/controllers/networks_controller.rb
+++ app/controllers/networks_controller.rb
@@ -2,7 +2,7 @@ class NetworksController < ApplicationController
   before_action :set_network, only: %i[show]
 
   def index
-    @networks = Network.where(country: @country)
+    @networks = Network.where(country: @country).order(name: :desc)
 
     expires_in 0.seconds, public: true
     fresh_when @networks
